7ceefd64a5edcf0748ac95f25df65826a34c6df5
Mike J Innes <mike.j.innes@gmail.com>
2020-05-12 18:48:47 +0100

    update shorthand

--- src/rules.jl
+++ src/rules.jl
@@ -14,3 +14,11 @@ function apply(o::Descent, x, x̄, state)
   η = convert(eltype(x̄), o.eta)
   x̄ .* η, state
 end
+
+function (o::Descent)(m, m̄)
+  update(o, m, m̄, state(o, m))[1]
+end
+
+function (o::Descent)(m, m̄, st)
+  update(o, m, m̄, st)
+end
