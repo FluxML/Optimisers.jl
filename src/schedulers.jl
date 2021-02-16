e9a9f639fdaf0d30ab6f127442e451eab6473393
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-02-16 23:40:07 +0530

    add docs

--- src/schedulers.jl
+++ src/schedulers.jl
@@ -1,14 +1,24 @@
-f(t, p) = (2 / π) * abs(asin(sin(π * (t - 1) / p)))
-sine(t, p = 1.) = (2 / π) * abs(asin(sin(π * (t - 1) / p)))
+# f(t, p) = (2 / π) * abs(asin(sin(π * (t - 1) / p)))
+# sine(t, p = 1.) = (2 / π) * abs(asin(sin(π * (t - 1) / p)))
+
+# Simple scheduling can happen as a basic closure.
 sine(p) = t -> (2 / Float32(π)) * abs(asin(sin(Float32(π) * (t - 1) / p)))
 
 mutable struct Schedule{O,F}
   f::F
   opt::O
   cursor::Float32
-  # cursor_step::Float64
 end
 
+"""
+    Schedule(f, opt)
+
+Create a scheduled optimiser whose update rule is controlled by `f`.
+
+`f` can be any callable, while `opt` can be any optimiser.
+
+See also [next](@ref), [init](@ref)
+"""
 function Schedule(f, opt)
   Schedule(f, opt, 1.f0 + eps(Float32)) 
 end
@@ -21,6 +31,14 @@ end
 # Most likely learning rate
 # If `f` relies on only time - it can be generated on the fly
 
+"""
+    next(s::Schedule, state)
+
+Returns a new optimiser as described by the scheduler function
+and the optimiser. This allocates a new optimiser on the stack, keeping the original one intact.
+
+The state is a tuple of the state as defined by the scheduling function.
+"""
 function next(s::Schedule{O}, (cursor, cursor_step)) where O
   cursor += cursor_step
   ADAM(s.opt, eta = s.f(cursor) * s.opt.eta) #replace with O(..)
@@ -32,7 +50,6 @@ init(s::Schedule, x) = (init(s.f, x), init(s.opt, x))
 function apply(s::Schedule, x, dx, st)
   schedst, optst = st
   o = next(s, schedst)
-  # @show o
   Δ, optst2 = apply(o, x, dx, optst)
   Δ, ((cursor .+ cursor_step, cursor_step), optst2)
 end
@@ -45,3 +62,5 @@ init(inv::InvDecay, x) = (1, 1)
 (inv::InvDecay)(t) = 1 / (1 + inv.decay * t)
 
 InvDecay(s = 0.1f0) = InvDecay{typeof(s)}(s)
+
+
