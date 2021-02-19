dc00a5421e271ff9da39237303903975bf1d5b95
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-02-19 23:25:39 +0530

    basic iterator support

--- src/schedulers.jl
+++ src/schedulers.jl
@@ -1,8 +1,15 @@
+using Base.Iterators: Cycle, Zip, Stateful,
+                      Repeated, Take, TakeWhile
+
 # f(t, p) = (2 / π) * abs(asin(sin(π * (t - 1) / p)))
 # sine(t, p = 1.) = (2 / π) * abs(asin(sin(π * (t - 1) / p)))
 
 # Simple scheduling can happen as a basic closure.
 triangle(t) = (1 - 2 * abs(round(Int, t/2) - t/2))
+
+const BaseIterators = Union{Cycle, Zip,
+                            Stateful, Repeated,
+                            Take, TakeWhile}
 
 mutable struct Schedule{O,F}
   f::F
@@ -23,7 +30,6 @@ function Schedule(f, opt)
   Schedule(f, opt, 1.f0 + eps(Float32)) 
 end
 
-
 # Timestep: t - always depends on run
 # Phase: p - mostly likely constant
 
@@ -31,26 +37,44 @@ end
 # Most likely learning rate
 # If `f` relies on only time - it can be generated on the fly
 
+# st = (t, timestep)
+function next(itr::T, st) where T <: BaseIterators
+  t, ts = st
+  @show st
+  iterate(itr, ts)
+end
+
+function next(arr::T, st) where T
+  t, ts = st
+  iterate(arr, ts)
+end
+
+next(f, (t,ts)) = f(t .+ ts), (t .+ ts, ts)
+
+next_opt(o::T, kwargs...) where T = T(o, kwargs...)
+
 """
     next(s::Schedule, state)
 
 Returns a new optimiser as described by the scheduler function
 and the optimiser. This allocates a new optimiser on the stack, keeping the original one intact.
 
-The state is a tuple of the state as defined by the scheduling function.
+The state is a tuple of the state as defined by the scheduling policy.
 """
-function next(s::Schedule{O}, (cursor, cursor_step)) where O
-  cursor += cursor_step
-  ADAM(s.opt, eta = s.f(cursor) * s.opt.eta), (cursor .+ cursor_step, cursor_step) #replace with O(..)
+function next(s::Schedule{O}, st) where O
+  snew, schedst = next(s.f, st)
+  @show schedst
+  ADAM(s.opt, eta = snew * s.opt.eta), (snew, schedst) #replace with O(..)
 end
 
-init(f, x) = (1.f0, 0.1f0)
+init(f, x) = (1, 1)
+init(itr::T, x) where T <: BaseIterators = iterate(itr)
 init(s::Schedule, x) = (init(s.f, x), init(s.opt, x))
 
 function apply(s::Schedule, x, dx, st)
   schedst, optst = st
-  # cursor, cursor_step = schedst
   o, schedst2 = next(s, schedst)
+  @show o
   Δ, optst2 = apply(o, x, dx, optst)
   Δ, (schedst2, optst2)
 end
@@ -60,7 +84,6 @@ struct InvDecay{T}
 end
 
 # (start, step) -> consider step to be a function/ vector of step sizes?
-init(inv::InvDecay, x) = (1, 1)
 (inv::InvDecay)(t) = 1 / (1 + inv.decay * t)
 
 InvDecay(s = 0.1f0) = InvDecay{typeof(s)}(s)
