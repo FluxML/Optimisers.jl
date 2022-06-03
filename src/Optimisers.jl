a658f1d5acaaa8c24b4599e5f1e508f9dd0d2392
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-06-03 11:05:03 -0400

    rm trainable, add link, add destructure

--- src/Optimisers.jl
+++ src/Optimisers.jl
@@ -70,26 +70,27 @@ julia> Optimisers.setup(Momentum(), m)  # same field names as m
 ```
 
 The recursion into structures uses Functors.jl, and any new `struct`s containing parameters
-need to be marked with `Functors.@functor` before use. Further refinements can, if necessary,
-be made by overloading [`trainable`](@ref).
+need to be marked with `Functors.@functor` before use.
+See [the Flux docs](https://fluxml.ai/Flux.jl/stable/models/advanced/) for more about this.
 
 ```jldoctest
-julia> struct Layer; mat; vec; end
+julia> struct Layer; mat; fun; end
 
-julia> model = (lay = Layer(rand(2, 2), rand(2)), fun = sin);
+julia> model = (lay = Layer([1 2; 3 4f0], sin), vec = [5, 6f0]);
 
 julia> Optimisers.setup(Momentum(), model)  # new struct is by default ignored
-(lay = nothing, fun = nothing)
+(lay = nothing, vec = Leaf(Momentum{Float32}(0.01, 0.9), [0.0, 0.0]))
+
+julia> destructure(model)
+(Float32[5.0, 6.0], Restructure(NamedTuple, ..., 2))
 
 julia> using Functors; @functor Layer  # annotate this type as containing parameters
 
 julia> Optimisers.setup(Momentum(), model)
-(lay = (mat = Leaf(Momentum{Float32}(0.01, 0.9), [0.0 0.0; 0.0 0.0]), vec = Leaf(Momentum{Float32}(0.01, 0.9), [0.0, 0.0])), fun = nothing)
+(lay = (mat = Leaf(Momentum{Float32}(0.01, 0.9), [0.0 0.0; 0.0 0.0]), fun = nothing), vec = Leaf(Momentum{Float32}(0.01, 0.9), [0.0, 0.0]))
 
-julia> Optimisers.trainable(l::Layer) = (vec = l.vec,)  # if necessary, ignore some parameters
-
-julia> Optimisers.setup(Momentum(), model)
-(lay = (mat = nothing, vec = Leaf(Momentum{Float32}(0.01, 0.9), [0.0, 0.0])), fun = nothing)
+julia> destructure(model)
+(Float32[1.0, 3.0, 2.0, 4.0, 5.0, 6.0], Restructure(NamedTuple, ..., 6))
 ```
 """
 setup
