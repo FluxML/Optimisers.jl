8c1c0734367bc62967819e6214c3e1183c0bf65a
Vedant Puri <vedantpuri@gmail.com>
2024-11-12 10:45:33 -0500

    edit warning to indicate that this is a correctness issue

--- ext/OptimisersAdaptExt.jl
+++ ext/OptimisersAdaptExt.jl
@@ -5,8 +5,9 @@ import Optimisers: Leaf
 
 function Adapt.adapt_structure(to, leaf::Leaf)
   @warn """`Optimisers.Leaf` object does not support device transfer via
-  `Adapt.jl`. Avoid this by calling `Flux.gpu/cpu` or
-  `MLDataDevices.cpu_device()/gpu_device()` on the optimiser state object.
+  `Adapt.jl`. This could lead to incorrect gradient updates. Avoid this by
+  calling `Flux.gpu/cpu` or `MLDataDevices.cpu_device()/gpu_device()` on the
+  optimiser state object.
   """ maxlog=1
 
   rule = Adapt.adapt(to, leaf.rule)
