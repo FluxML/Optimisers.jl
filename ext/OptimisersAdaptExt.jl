978ed6f4eae688ceb671c5aa8f2e74f4a2e881b3
Vedant Puri <vedantpuri@gmail.com>
2024-11-12 12:37:21 -0500

    Update ext/OptimisersAdaptExt.jl
    Co-authored-by: Michael Abbott <32575566+mcabbott@users.noreply.github.com>

--- ext/OptimisersAdaptExt.jl
+++ ext/OptimisersAdaptExt.jl
@@ -5,8 +5,9 @@ import Optimisers: Leaf
 
 function Adapt.adapt_structure(to, leaf::Leaf)
   @warn """`Optimisers.Leaf` object does not support device transfer via
-  `Adapt.jl`. This could lead to incorrect gradient updates. Avoid this by
-  calling `Flux.gpu/cpu` or `MLDataDevices.cpu_device()/gpu_device()` on the
+  `Adapt.jl`. This is because `Adapt.jl` does not handle shared parameters (i.e. the same parameter array
+  appearing more than once in the model), and in such cases this will lead to  incorrect gradient updates. 
+  Avoid this by calling `Flux.gpu/cpu` or `MLDataDevices.cpu_device()/gpu_device()` on the
   optimiser state object.
   """ maxlog=1
 
