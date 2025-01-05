8b94d00d63d76993d2fce3a0685f63eadafed8d7
William Moses <gh@wsmoses.com>
2025-01-05 14:01:35 -0500

    Update src/utils.jl
    Co-authored-by: Michael Abbott <32575566+mcabbott@users.noreply.github.com>

--- src/utils.jl
+++ src/utils.jl
@@ -15,8 +15,15 @@ end
 
 ofeltype(x, y) = convert(float(eltype(x)), y)
 
-_eps(T::Type{<:AbstractFloat}, e) = T(e)
-# catch complex and integers
+"""
+    _eps(Type{T}, val)
+
+Mostly this produces `real(T)(val)`, so that `_eps(Float32, 1e-8) === 1f-8` will
+convert the Float64 parameter epsilon to work nicely with Float32 parameter arrays.
+
+But for Float16, it imposes a minimum of `Float16(1e-7)`, unless `val==0`.
+This is basically a hack to increase the default epsilon, to help many optimisers avoid NaN.
+"""
 _eps(T::Type{<:Number}, e) = real(float(T))(e) 
 # avoid small e being rounded to zero
 _eps(T::Type{Float16}, e) = e == 0 ? T(0) : max(T(1e-7), T(e))
