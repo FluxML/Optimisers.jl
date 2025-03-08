3cc71674d9e3add91d19ba5014608cdb916e00cd
Avik Pal <avikpal@mit.edu>
2025-03-08 13:33:02 -0500

    fix: add a way to override eta check for reactant

--- src/interface.jl
+++ src/interface.jl
@@ -270,7 +270,7 @@ macro def(expr)
   rule = Meta.isexpr(expr.args[2], :<:) ? expr.args[2].args[1] : expr.args[2]
   expr.args[2] = Expr(:(<:), Expr(:curly, rule, type_params...), :AbstractRule)
   params = [Expr(:kw, nv...) for nv in zip(names, default_vals)]
-  check_sign_eta = :eta in names ? :(eta < 0 && throw(DomainError(eta, "the learning rate cannot be negative"))) : nothing
+  check_sign_eta = :eta in names ? :($(_assert_positive_eta)(eta)) : nothing
   # Positional-argument method, has defaults for all but the first arg:
   positional = :(function $rule($(names[1]), $(params[2:end]...))
     vars = $(_def_check_type).([$(names...)],[$(default_types...)])
@@ -292,6 +292,11 @@ macro def(expr)
             print(io, $rule,"(", join(pairs, ", "), ")")
           end
         end |> esc
+end
+
+_assert_positive_eta(eta) = _assert_positive_eta(eta, eta < 0)
+function _assert_positive_eta(eta, cond::Bool)
+  cond && throw(DomainError(eta, "the learning rate cannot be negative"))
 end
 
 _def_typeof(val) = typeof(val)
